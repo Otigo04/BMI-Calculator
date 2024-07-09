dc710851205a35ab08cd1f41ea55f0ca2aab44a5
Orhan Tas <orhantas3818@outlook.de>
2024-07-09 20:01:33 +0200

    V0.3
    - fixed an ArrayOutOfBoundsException Error occuring when BMI value was over 27 i think. Should be working fine now!

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    static BMIInfo[][] bmiInfoMen = {
-            {
-                    new BMIInfo(Double.NEGATIVE_INFINITY, 19, "Du bist untergewichtig!"),
-                    new BMIInfo(19, 24, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
-                    new BMIInfo(25, 28, "Das ist dein Normalgewichtig!"),
-                    new BMIInfo(28, Double.POSITIVE_INFINITY, "Du bist übergewichtig!")
-            },
-            {
-                    new BMIInfo(Double.NEGATIVE_INFINITY, 20, "Du bist untergewichtig!"),
-                    new BMIInfo(20, 25, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
-                    new BMIInfo(26, 29, "Das ist dein Normalgewicht!"),
-                    new BMIInfo(29, Double.POSITIVE_INFINITY, "Du bist übergewichtig!")
-            },
-    };
-
-    static BMIInfo[][] bmiInfoWomen = {
-            {
-                    new BMIInfo(Double.NEGATIVE_INFINITY, 18, "Du bist untergewichtig!"),
-                    new BMIInfo(20, 25, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
-                    new BMIInfo(26, 29, "Du leidest an leichtem Übergewicht!"),
-                    new BMIInfo(29, Double.POSITIVE_INFINITY, "Du bist stark übergewichtig!")
-            },
-            {
-                    new BMIInfo(Double.NEGATIVE_INFINITY, 18, "Du bist untergewichtig!"),
-                    new BMIInfo(20, 25, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
-                    new BMIInfo(26, 29, "Du leidest an leichtem Übergewicht!"),
-                    new BMIInfo(29, Double.POSITIVE_INFINITY, "Du bist stark übergewichtig!")
-            }
-    };
-
     public static void main(String[] args) {
-        //KOMMENTIEREN NICHT VERGESSEN!!!
-
         Input inputs = new Input();
         BMICalc calc = new BMICalc();
-        // Methodcall from "Input" class
         inputs.startInput();
         inputs.ageInput();
         inputs.genderInput();
         inputs.weightInput();
         inputs.heightInput();
 
-        // "Rechner" calculates the given weight / height²
         double bmiResult = calc.calculator(inputs.weightInput, inputs.heightInput);
-        System.out.println(Math.round(bmiResult * 100.0) / 100.0); // Rundet auf zwei Nachkommastellen
+        System.out.print(Math.round(bmiResult * 100.0) / 100.0);
 
-        BMIInfo[] bmiInfos = getBmiInfos(inputs);
+        int[] ageRanges = {24, 34, 44, 54, 64, 90};
 
+        // 2D arrays to hold BMI information for men and women
+        BMIInfo[][] bmiInfoMen = {
+                {
+                        new BMIInfo(Double.NEGATIVE_INFINITY, 19, "Du bist untergewichtig!"),
+                        new BMIInfo(19, 24, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
+                        new BMIInfo(25, 28, "Du leidest an leichtem Übergewicht!"),
+                        new BMIInfo(28, Double.POSITIVE_INFINITY, "Du bist übergewichtig!")
+                },
+                {
+                        new BMIInfo(Double.NEGATIVE_INFINITY, 20, "Du bist untergewichtig!"),
+                        new BMIInfo(20, 25, "Das ist dein Normalgewicht! Herzlichen Glückwunsch!"),
+                        new BMIInfo(26, 29, "Du leidest an leichtem Übergewicht!"),
+                        new BMIInfo(29, Double.POSITIVE_INFINITY, "Du bist übergewichtig!")
+                }
+            };
 
-        // passende Nachricht finden
-        for (BMIInfo bmiInfo : bmiInfos) {
-            if (bmiResult >= bmiInfo.lowerBound && bmiResult < bmiInfo.upperBound) {
-                System.out.println(bmiInfo.message);
-                break;
-            }
-        }
-    }
+        BMIInfo[][] bmiInfoWomen = {
+                {
+                        new BMIInfo(Double.NEGATIVE_INFINITY, 18, "Du bist untergewichtig!"),
+                        new BMIInfo(18, 24, "Das ist dein Normalegewicht! Herzlichen Glückwunsch!"),
+                        new BMIInfo(24, 29, "Du bist leicht übergewichtig!"),
+                        new BMIInfo(29, 39, "Du bist übergewichtig!"),
+                        new BMIInfo(39, Double.POSITIVE_INFINITY, "Du bist stark übergewichtig!")
+                },
+                {
+                        new BMIInfo(Double.NEGATIVE_INFINITY, 19, "Du bist untergewichtig!"),
+                        new BMIInfo(19, 25, "Das ist dein Normalegewicht! Herzlichen Glückwunsch!"),
+                        new BMIInfo(25, 30, "Du bist leicht übergewichtig"),
+                        new BMIInfo(30, 40, "Du bist übergewichtig!"),
+                        new BMIInfo(40, Double.POSITIVE_INFINITY, "Du bist stark übergewichtig!")
+                }
+        };
 
-    private static BMIInfo[] getBmiInfos(Input inputs) {
-        int[] ageRanges = {24, 34, 44, 54, 64, 90}; // Obergrenze der Altersbereiche
-
-        // Richtigen Altersbereich finden
         int ageIndex = 0;
         while (ageIndex < ageRanges.length && inputs.age > ageRanges[ageIndex]) {
             ageIndex++;
         }
 
-        // Richtige BMI-Infos basierend auf Geschlecht und Alter
+        // Handle case where ageIndex might be out of bounds
+        if (ageIndex >= bmiInfoMen.length) {
+            ageIndex = bmiInfoMen.length - 1;
+        }
+
         BMIInfo[] bmiInfos;
         if (inputs.isMan) {
             bmiInfos = bmiInfoMen[ageIndex];
         } else {
             bmiInfos = bmiInfoWomen[ageIndex];
         }
-        return bmiInfos;
+
+        for (BMIInfo info : bmiInfos) {
+            if (bmiResult >= info.lowerBound && bmiResult < info.upperBound) {
+                System.out.print(", " + info.message);
+                break;
+            }
+        }
     }
 }
